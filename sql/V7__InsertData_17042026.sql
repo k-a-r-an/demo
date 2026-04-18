fa35a305d761a60800d18f3eaa1d88fc69d5f5cc
Karan <karanmore414@gmail.com>
2026-04-18 16:05:14 +0530

    don

---
+++ sql/V7__InsertData_17042026.sql
@@ -0,0 +1,3 @@
+insert into customerdata.tbl_patient(patient_id,patient_name_first,patient_name_last,patient_middle_initial,patient_dob,patient_gender,country_id,deactivation_date,created_by,modified_by)
+values
+(1,'Karan','More','A','05/JULY/2001','M','36',null,'SYSTEM','SYSTEM')
